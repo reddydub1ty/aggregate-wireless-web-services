22d1a620ba9b20c37b5909fda6c5742e482a8d41
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-09-22 01:02:23 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,26 @@
+}
+        int[] data = new int[10];
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }
+
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+
+            System.out.println("Random Number: " + item);
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    main()
+def main():
+
+if __name__ == "__main__":
+    data = generate_random_data()
+import random
+    for item in data:
+
+        print(f"Random Number: {item}")
